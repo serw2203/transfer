99f1a0d8359896d6207aa45879de6fc600af5f80
Serg <serw2203@yandex.ru>
2016-05-06 20:46:24 +0300

    add dep

--- src/main/java/ru/transfer/services/OperationServiceImpl.java
+++ src/main/java/ru/transfer/services/OperationServiceImpl.java
@@ -1,7 +1,9 @@
 package ru.transfer.services;
 
+import ru.transfer.model.Operation;
+
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -12,11 +14,13 @@ import javax.ws.rs.core.Response;
  */
 @Path("/")
 public class OperationServiceImpl {
+
     @GET
     @Path("/operation/")
     @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-    public Response operation() {
+    @Consumes ({MediaType.APPLICATION_JSON})
+    public Response operation(Operation operation) {
         return Response.status(200).header("content-type", "text/plain; charset=utf-8")
-                .entity("DONE !!!").build();
+                .entity(operation).build();
     }
 }
